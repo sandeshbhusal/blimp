cdb74d307c491834e402836663c46f4de3cc686a
Sandesh Bhusal <sandeshbhusal@mordor.boisestate.edu>
2024-11-12 17:18:44 -0700

    Very small a+b function in cranelift

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
-use std::collections::HashMap;
 use cranelift::prelude::Value;
+use std::collections::HashMap;
 
-use cranelift::{jit::{JITBuilder, JITModule}, module::{Linkage, Module}, prelude::{settings, types::{self, I32}, AbiParam, FunctionBuilder, FunctionBuilderContext, InstBuilder}};
+use cranelift::{
+    jit::{JITBuilder, JITModule},
+    module::{Linkage, Module},
+    prelude::{
+        settings,
+        types::{self, I32},
+        AbiParam, FunctionBuilder, FunctionBuilderContext, InstBuilder,
+    },
+};
 
 enum Op {
     Plus,
@@ -54,53 +62,51 @@ fn gather_identifiers(expr: &Expr) -> Vec<String> {
     };
 }
 
-fn eval(){}
+fn eval() {}
 
-fn compile(expr: Expr) -> i32 {
+type rvalType = extern "C" fn(i32, i32) -> i32;
+fn compile(expr: Expr) -> rvalType {
     let ids = gather_identifiers(&expr);
 
-    // Compile to cranelift JIT.
-    let mut jit_builder = JITBuilder::new(cranelift::module::default_libcall_names()).unwrap();
+    let jit_builder = JITBuilder::new(cranelift::module::default_libcall_names()).unwrap();
     let mut jit_module = JITModule::new(jit_builder);
     let mut context = jit_module.make_context();
     let mut func_context = FunctionBuilderContext::new();
 
     let mut signatures = jit_module.make_signature();
-    for id in ids {
-        // TODO: Modify with types.
+    for _ in 0..2 {
         signatures.params.push(AbiParam::new(types::I32));
     }
     signatures.returns.push(AbiParam::new(types::I32));
 
-    let func = jit_module.declare_function("check", Linkage::Local, &signatures).unwrap();
+    let func = jit_module
+        .declare_function("check", Linkage::Local, &signatures)
+        .unwrap();
+
+    context.func.signature = signatures;
     let mut fb = FunctionBuilder::new(&mut context.func, &mut func_context);
     {
-
-        let mut block = fb.create_block();
+        let block = fb.create_block();
         fb.switch_to_block(block);
-        let rval = fb.ins().iconst(types::I32, 42);
-        let rstmt = fb.ins().return_(&[rval]);
-        fb.seal_all_blocks();
-        fb.finalize();
+        fb.append_block_params_for_function_params(block);
+        let first = fb.block_params(block)[0];
+        let second = fb.block_params(block)[1];
+        let rval = fb.ins().iadd(first, second);
+        fb.ins().return_(&[rval]);
     }
 
     jit_module.define_function(func, &mut context).unwrap();
     jit_module.clear_context(&mut context);
     jit_module.finalize_definitions().unwrap();
 
-    type rvalType = extern "C" fn (i32, i32) -> i32;
     let code = jit_module.get_finalized_function(func);
-    let ptr_b = unsafe {
-        std::mem::transmute::<_, rvalType>(code)
-    };
+    let ptr_b = unsafe { std::mem::transmute::<_, rvalType>(code) };
 
-    let rval = ptr_b(10, 11);
-    rval
+    ptr_b
 }
 
 fn main() {
-
-    println!("{}", compile(Expr::I32(10)));
+    println!("{}", compile(Expr::I32(10))(10, 100000000));
 
     // let expr = "x1 < x2 || x1 ** 2 == x2";
 
